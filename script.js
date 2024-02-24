76e136e92531b635ff2a64efb4e5f2c3ba3d20d2
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-02-24 12:34:15 +0300

    Mark finished task

--- script.js
+++ script.js
@@ -19,14 +19,28 @@ function addToDo(){
         itemList.innerText= inputText;
         inputList.appendChild(itemList);
 
+
         //Delete task from list   
         const deleteButton = document.createElement("button"); 
-        deleteButton.innerText="X";
+        deleteButton.innerText="Del";
         itemList.appendChild(deleteButton);
         deleteButton.style.marginRight = '5px';
+        deleteButton.style.border = 'none';
 
         deleteButton.addEventListener('click', function(){
             inputList.removeChild(itemList); 
+        })
+        
+        //Mark done item
+        const markButton = document.createElement("button"); 
+        markButton.innerHTML='&#x2713;';
+        itemList.appendChild(markButton);
+        markButton.style.cssText = "margin-right: 20px; border: none;"
+        // markButton.style.marginRight = '20px';
+        // markButton.style.border = 'none';
+
+        markButton.addEventListener('click', function(){
+            itemList.style.textDecoration= "line-through";
         })
 
         //clear input value
