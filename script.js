c5581750c1b9cd136cbc5715670889f4ba5304ae
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-02-24 11:42:12 +0300

    Updated the app.js

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
 function addToDo(){
-    //Access input value
-    let inputTask=document.getElementById("task-input");
-    let inputText = inputTask.value;
+    const addBtn = document.getElementById('add-task-btn');
 
-    // Check if no task is entered.
-    if(inputText===""){
-        alert("Enter a task please");
-        return
-    }
+    addBtn.addEventListener('click', function(){
 
-    //Add task to the list
-    let inputList = document.getElementById("task-list");    
-    let itemList=document.createElement("li");
-    itemList.innerText= inputText;
-    inputList.appendChild(itemList);
+        //Access input value
+        let inputTask=document.getElementById("task-input");
+        let inputText = inputTask.value;
 
-    //Delete task from list
-    let deleteButton = document.createElement("button");
-    deleteButton.innerText="X";
-    itemList.appendChild(deleteButton);
-
-    deleteButton.addEventListener('click', function(){
-        inputList.removeChild(itemList); 
-    })    
-
-    //clear input value
-    inputTask.value="";   
-    let count = 0;
-    let taskList = inputList.querySelectorAll("li");
-
-    for (let i=0; i<taskList.length;i++){
-        count++;
-        let countDisplay = document.getElementById("task-count");
-        countDisplay.innerText="You have " + count + " tasks to attend to."
-    }
+        // Check if no task is entered
+        if(inputText===""){
+            alert("Enter a task please");
+            return
+        }
     
-   
+        //Add task to the list
+        let inputList = document.getElementById("task-list");    
+        let itemList=document.createElement("li");
+        itemList.innerText= inputText;
+        inputList.appendChild(itemList);
+
+        //Delete task from list   
+        const deleteButton = document.createElement("button"); 
+        deleteButton.innerText="X";
+        itemList.appendChild(deleteButton);
+        deleteButton.style.marginRight = '5px';
+
+        deleteButton.addEventListener('click', function(){
+            inputList.removeChild(itemList); 
+        })
+
+        //clear input value
+        inputTask.value="";   
+    })        
 }
+
+addToDo();
